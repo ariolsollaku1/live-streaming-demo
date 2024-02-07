5cea31349286e17b899ffc95247e08684e234f4d
Galapchev <135614720+Galapchev@users.noreply.github.com>
2024-02-07 15:59:41 +0100

    Update app.js
    updated gpt model settings

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.post('/openai', async function (req, res, next) {
 
     console.log(req.body.messages)
   let resp = await axios.post('https://api.openai.com/v1/chat/completions', {
-    "model": "gpt-3.5-turbo-16k",
+    "model": "gpt-4-turbo-preview",
     "messages": req.body.messages,
-    "temperature": 0.8,
-    "max_tokens": 256,
+    "temperature": 0.67,
+    "max_tokens": 420,
     "top_p": 1,
-    "frequency_penalty": 0.49,
-    "presence_penalty": 0.23,
+    "frequency_penalty": 0.1,
+    "presence_penalty": 0.12,
 
   },{
     headers: {
